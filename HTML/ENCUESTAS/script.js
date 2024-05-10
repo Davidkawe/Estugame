71c4c346ae1e18b11fa4bf7ca56d299404e54d54
DESKTOP-HTV43K4\david <david.jmnz.fndz@gmail.com>
2024-05-10 13:00:38 +0200

    Actualización masiva

--- HTML/ENCUESTAS/script.js
+++ HTML/ENCUESTAS/script.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function slides()
     let currentSlide = 0;
     const slides = document.querySelectorAll('.slide');
     const totalSlides = slides.length;
+    let votoPst = 0;
+    let votoNgv = 0;
 
     function showSlide(index)
     {
@@ -15,14 +17,35 @@ document.addEventListener('DOMContentLoaded', function slides()
 
     function nextSlide()
     {
+        
         currentSlide = (currentSlide + 1) % totalSlides;
         showSlide(currentSlide);
     }
 
+    function Pst_Ngt(event)
+    {
+        if (event.target.id = 'positiveBtn'){
+            votoPst = 1;
+            votoNgv = 0;
+        } else if (event.target.id = 'negativeBtn'){
+            votoPst = 0;
+            votoNgv = 1;
+        }
+
+        $.ajax({
+            URL: "index.php",
+            method: "POST",
+            data: {votoNgv, votoPst},
+            success: function(data){
+                console.log(data);
+            }
+        })
+    }
+
     showSlide(currentSlide);
 
-    document.getElementById('positiveBtn').addEventListener('click', nextSlide);
-    document.getElementById('negativeBtn').addEventListener('click', nextSlide);
+    document.getElementById('positiveBtn').addEventListener('click', nextSlide, Pst_Ngt);
+    document.getElementById('negativeBtn').addEventListener('click', nextSlide, Pst_Ngt);
 });
 
 $("i").click(function () {
